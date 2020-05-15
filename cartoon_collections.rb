0eee26036017411befc9d5504eb464cdbc0f8df6
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-15 05:08:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,9 @@ def long_planeteer_calls(calls)
   calls.any? {|call| call.length > 4} 
 end
 
-def find_the_cheese(cheeses)
+def find_the_cheese(maybe_cheeses)
   cheeses = %w[gouda cheddar camembert]
-  cheeses.find do |cheese|
+  maybe_cheeses.find do |cheese|
   cheese.include? (cheese)
   # cheeses_types = ["cheddar", "gouda", "camembert"]
  end
