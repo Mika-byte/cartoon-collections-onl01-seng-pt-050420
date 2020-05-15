72b0d71c7919093034e1e7f632e95b84a70d1e86
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-15 04:27:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  # Your code here
+ puts "#{index + 1} #{name}"
 end
 
 def long_planeteer_calls# code an argument here
