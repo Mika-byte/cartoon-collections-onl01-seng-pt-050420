2d2c47e2fa897fdf9fdd3374b8a354c2bf8fb187
Michaelle Barbor <michaelle.barbor@outlook.com>
2020-05-15 04:36:22 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,10 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
- 
+ planeteer_calls.map do |name|
+   name.capitalize << "!"
+ end
+   
 end
 
 def long_planeteer_calls# code an argument here
